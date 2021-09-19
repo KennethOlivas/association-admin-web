7a91e4ae2029c079e326b7dcaf7c01b3c1c8f2cc
Kenneth Olivas <kennetholivas1@gmail.com>
2021-09-18 22:06:30 -0300

    Update api.js

--- src/lib/api.js
+++ src/lib/api.js
@@ -3,7 +3,7 @@ import {access_token} from '../store/session'
 import {newAccessToken} from "./utils";
 
 
-export const urlBase = "http://192.168.107.200:1337";
+export const urlBase = "https://young-ocean-73880.herokuapp.com";
 
 export const get = (url) => {
     const urlComplete = urlBase + url;
