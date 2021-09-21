ed5c3f6c6f55384d9c09ee98bba0cf18427fef87
Kenneth Olivas <49175490+KennethOlivas@users.noreply.github.com>
2021-09-20 18:10:53 -0600

    api get employee

--- src/routes/empleados/index.svelte
+++ src/routes/empleados/index.svelte
@@ -1,35 +1,69 @@
 <script>
 	import Table from '../../components/Table.svelte';
 	import Modal from '../../components/Modal.svelte';
+	import * as api from '../../lib/api';
+	import { onMount } from 'svelte';
+	import Loader from '../../components/Loader.svelte';
+
 	let head = [];
 	let body = [];
-	head = ['id', 'nombre', 'apellido'];
-	body = ['9', 'kenneth', 'olivas'];
-
+	head = ['id', 'nombre', 'nacionalidad'];
+	let tableControler
+	let employees
+	let loading = false
+	onMount(async () => {
+		try {
+			await getEmployees()
+		} catch (error) {
+			
+		}
+		loading = true
+	})
     
+	const getEmployees = async() =>{
+		let res = await api.get('/employees').then((response) => response.json());
+		employees = [];
+		employees = [...res];
+		console.log(employees);
+	}
+
+
+	const loaData = () => {
+		body = [];
+		for (const data of employees) {
+			body.push([data.id, data.name, data.nationality]);
+		}
+		
+		tableControler.addDataBody(body);
+	};
 
 </script>
 
 <h1 class="text-center text-2xl font-bold text-gray-700">Empleados</h1>
-
-<Modal tilte="Agregar usuario" btnName="Agregar usuario">
-	<label for="username" class="label">
-		<span class="label-text">Nombre de empleado </span>
-	</label>
-	<input
-		type="text"
-		ud="username"
-		placeholder="Nombre usuario"
-		class="input input-primary input-bordered focus:placeholder-primary"
+<div class="flex items-center justify-end">
+	<Modal tilte="Agregar empleado" btnName="Agregar">
+		<label for="username" class="label">
+			<span class="label-text">Nombre de empleado </span>
+		</label>
+		<input
+			type="text"
+			ud="username"
+			placeholder="Nombre usuario"
+			class="input input-primary input-bordered focus:placeholder-primary"
+		/>
+		<div class="modal-action">
+			<button class="btn btn-primary w-1/2" type="button"><label for="my-modal-2" class="">Agregar</label></button>
+			<label for="my-modal-2" class="btn w-1/2">Cancelar</label>
+		</div>
+	</Modal>
+</div>
+{#if loading}
+	<Table
+		bind:this={tableControler}
+		{head}
+		on:message={loaData}
+	
 	/>
-	<div class="modal-action">
-		<button class="btn btn-primary w-1/2" type="button"><label for="my-modal-2" class="">Agregar</label></button>
-		<label for="my-modal-2" class="btn w-1/2">Cancelar</label>
-	</div>
-</Modal>
-
-<Table {head} {body}>
-	<button class="btn  btn-ghost btn-circle btn-sm mx-1 "><i class="fas fa-pen" /></button>
-
-	<button class="btn  btn-ghost btn-circle btn-sm mx-1"><i class="fas fa-trash-alt" /></button>
-</Table>
+{:else}
+	<Loader />
+{/if}
