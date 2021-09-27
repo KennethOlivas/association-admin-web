8afa69a0a2a41e4ee21208d1b707be118d97fdee
Kenneth Olivas <kennetholivas1@gmail.com>
2021-09-27 13:50:31 -0300

    localStorage is not defined

--- src/store/session.js
+++ src/store/session.js
@@ -1,47 +1,64 @@
-import {writable} from 'svelte/store';
+import { writable } from 'svelte/store';
+import { amp, browser, dev, mode, prerendering } from '$app/env';
 
 export const createAccessTokenStore = () => {
-    const { set, update, subscribe } = writable(getAccessToken())
+	const { set, update, subscribe } = writable(getAccessToken());
 
-    return {
-        subscribe,
-        reset: (value) => {
-            set(value)
-            localStorage.setItem("access_token", value)
-        }
-    }
-}
+	return {
+		subscribe,
+		reset: (value) => {
+			set(value);
+			if (browser) {
+				localStorage.setItem('access_token', value);
+			}
+		}
+	};
+};
 
 export const createRefreshTokenStore = () => {
-    const { set, update, subscribe } = writable(getRefreshToken())
+	const { set, update, subscribe } = writable(getRefreshToken());
 
-    return {
-        subscribe,
-        reset: (value) => {
-            set(value)
-            localStorage.setItem("refresh_token", value)
-        }
-    }
-}
+	return {
+		subscribe,
+		reset: (value) => {
+			set(value);
+			if (browser) {
+				localStorage.setItem('refresh_token', value);
+			}
+		}
+	};
+};
 
 export const createUserStore = () => {
-    const { set, update, subscribe } = writable(getUser())
+	const { set, update, subscribe } = writable(getUser());
 
-    return {
-        subscribe,
-        reset: (value) => {
-            set(value)
-            localStorage.setItem("user", JSON.stringify(value))
-        },
-    }
-}
+	return {
+		subscribe,
+		reset: (value) => {
+			set(value);
+			if (browser) {
+				localStorage.setItem('user', JSON.stringify(value));
+			}
+		}
+	};
+};
 
-const getAccessToken = () => localStorage.getItem("access_token") || ''
-const getRefreshToken = () => localStorage.getItem("refresh_token") || ''
-const getUser = () => JSON.parse(localStorage.getItem("user")) || {}
+const getAccessToken = () => {
+	if (browser) {
+		return localStorage.getItem('access_token');
+	}
+};
+const getRefreshToken = () => {
+	if (browser) {
+		return localStorage.getItem('refresh_token');
+	}
+};
+const getUser = () => {
+	if (browser) {
+		return JSON.parse(localStorage.getItem('user'));
+	}
+};
 
-
-
-export const access_token = createAccessTokenStore()
-export const refresh_token = createRefreshTokenStore()
-export const user = createUserStore()
+export const access_token = createAccessTokenStore();
+export const refresh_token = createRefreshTokenStore();
+export const user = createUserStore();
